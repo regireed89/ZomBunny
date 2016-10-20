de4644abf1583c1b8d5c11ac2b81c040ddd69674
regireed89 <regireed89@gmail.com>
2016-10-20 01:36:17 -0500

    ZomBunny

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+
+{
+	public int Score;
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log (Score);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Bleh ();
+	}
+
+	public void Bleh()
+	{
+		Score++;
+	}
+}
